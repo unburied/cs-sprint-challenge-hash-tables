20bee8eab9759f1ed795b808384755268790621b
David <davi86m@gmail.com>
2020-09-13 22:35:56 -0400

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,25 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    traverse tickets array
+    get info on ticket source and destination
+    add source and destination to dict
+    traverse dict, adding source info to a route array
     """
     # Your code here
+    route = [None] * length
+    info = {}
+    
+    # add ticket info to dict for easy lookup
+    for ticket in tickets:
+        info[ticket.source] = ticket.destination
+    
+    for i in range(length):
+        # first element is always pointing to NONE
+        if i == 0:
+            route[0] = info['NONE']
+        # each following element points to previous
+        else:
+            route[i] = info[route[i-1]]
 
     return route
