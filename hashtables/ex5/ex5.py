0af0c4a23141ef719953b3ab07d9e7f41dd037f6
David <davi86m@gmail.com>
2020-09-13 23:55:23 -0400

    ex5 done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -4,9 +4,31 @@
 
 def finder(files, queries):
     """
-    YOUR CODE HERE
+    Use dict to allow for easy lookup of file path
+    set up keys to be a last element of split file names
+    with that value as the original
+    THen use queries to check if any element of the 
+    query is in the key
     """
     # Your code here
+    paths = {}
+
+    for f in files:
+        # key is last element of file name
+        key = f.split('/')[-1]
+
+        # val is full file name, and there may be multpily paths with same name
+        if key in paths:
+            paths[key].append(f)
+        else:
+            paths[key] = [f]
+
+    # adds file path to result if the query is in the dict
+    result = []
+    
+    for q in queries:
+        if q in paths:
+            result.extend(paths[q])
 
     return result
 
