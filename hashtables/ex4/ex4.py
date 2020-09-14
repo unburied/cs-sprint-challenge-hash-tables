7ad3b58c42c31a5f52f2afe3206e70a7ca00985d
David <davi86m@gmail.com>
2020-09-13 23:20:40 -0400

    ex4 done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,24 @@
 def has_negatives(a):
     """
-    YOUR CODE HERE
+    Add all elements to the a dict with a false flag
+    itterate over the dict to check if each key made
+    negative is in the dict, if so assign a true
+    flag. Return all keys with val true
     """
     # Your code here
 
+    nums = {num:False for num in a}
+
+    for key in nums:
+        make_neg = key * -1
+        
+        # check if the positive value has a neg in the dict
+        if key > 0 and make_neg in nums:
+            nums[key] = True
+
+    # add all keys whose value is true to return list
+    result = [k for k,v in nums.items() if v]
+    
     return result
 
 
